0bf44f63ce1a57c2e9b8294a64bf0baf18e26055
Kornél Horváth <kornel.horvath@me.com>
2021-04-18 14:19:35 +0200

    Updated project files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
 	root: true,
 	extends: './index.js',
+	parserOptions: {
+		ecmaVersion: 2021, // 12
+		sourceType: 'script'
+	},
 	env: {
+		es2021: true,
 		node: true
 	},
 	rules: {
