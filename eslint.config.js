e67212fdfdfbbf1a5d2c6e344f5f56e9f6263c32
Kornél Horváth <kornel.horvath@me.com>
2025-06-30 23:45:44 +0200

    Added type declaration

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import koffeine from './src/index.js';
 import globals from 'globals';
+import typescriptParser from '@typescript-eslint/parser';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -14,6 +15,13 @@ export default [
 		files: [ 'test/**/*.js' ],
 		languageOptions: {
 			globals: globals.node
+		}
+	},
+	{
+		files: [ 'index.d.ts' ],
+		languageOptions: {
+			globals: globals.browser,
+			parser: typescriptParser
 		}
 	}
 ];
